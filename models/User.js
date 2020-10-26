2e980b44c7f441bb367a2071b189f8c5ce628da8
Nirav <nirav11190@hotmail.com>
2020-10-25 21:59:08 -0400

    server,database setup and client setup

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  //   ImageUrl: {
+  //       type: String,
+  //       require: true,
+  //       unique: true
+  //   }
+});
+
+module.exports = mongoose.model('user', UserSchema);

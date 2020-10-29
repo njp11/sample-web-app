303447b7258991af617f30eecce1bce281380afc
Nirav <nirav11190@hotmail.com>
2020-10-28 21:34:03 -0400

    typo fixed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) =>
-    res.sendFile(path.resilve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
 
